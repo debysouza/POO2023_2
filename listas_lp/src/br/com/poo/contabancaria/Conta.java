3c3079b796054c1ad1bf963880aab27b4de6f907
debysouza <deborasouza@id.uff.br>
2023-08-31 22:06:08 -0300

    Exercício da Conta Bancária.

---
+++ listas_lp/src/br/com/poo/contabancaria/Conta.java
@@ -0,0 +1,22 @@
+package br.com.poo.contabancaria;
+
+public class Conta {
+
+	int numero;
+	String titular;
+	double saldo;
+	
+	public Conta() {
+	}
+
+	public Conta(int numero, String titular, double saldo) {
+		this.numero = numero;
+		this.titular = titular;
+		this.saldo = saldo;
+	}
+
+	@Override
+	public String toString() {
+		return "BLA BLA BLA BLA BLA";
+	}
+}
